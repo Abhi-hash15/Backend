43e3f331dfd7dce8664a1b548db9444c90d2b7b6
abhinav kumar <abhijnv152006@gmail.com>
2025-12-29 10:22:01 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // require("dotenv").config({path:"./env"})
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
-dotenv.config({path:"./env"})
+dotenv.config({path:"./.env"})
+
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 3000, () => {
